040eee1218bd8901b9521a52620c62d887dcedf3
Blair MacIntyre <blair@macmynatt.com>
2021-08-12 11:27:50 -0400

    added sign in state to header

---
+++ _site/assets/javascripts/sso.js
@@ -0,0 +1,33 @@
+function maskEmail(email) {
+    if (!email) return "";
+    const emailParts = email.split("@");
+    const emailIdentity = emailParts[0];
+    const emailDomain = emailParts[1];
+    const truncatedIdentity = emailIdentity.substring(0, Math.min(emailIdentity.length, 3));
+    return `${truncatedIdentity}...@${emailDomain}`;
+  }
+  
+
+function updateLoginStatus(newValue) {
+    let div = document.querySelector('#login-status');
+    if (!newValue) {
+        newValue = window.localStorage.getItem("__ael_hubs_sso")
+    }
+    if (newValue) {
+
+        data = JSON.parse(e.newValue)
+        if (data.credentials && data.credentials.email) {
+            div.innerHTML = "Signed in as <em>" + maskEmail(data.credentials.email) + "</em>"
+            return;
+        } 
+    }
+    div.innerHTML = "Not signed in"
+}
+
+updateLoginStatus();
+
+window.addEventListener('storage', function(e) {
+    if (e.key === " __ael_hubs_sso") {
+        updateLoginStatus(e.newValue)           
+    }
+});
