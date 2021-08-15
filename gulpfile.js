7159d543e8e1ebd463136e3bc5d25d134921006b
Blair MacIntyre <blair@macmynatt.com>
2021-08-15 17:45:33 -0400

    update to recent gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass')(require('sass')),    autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     postcss = require('gulp-postcss'),
@@ -12,85 +11,84 @@ var gulp = require('gulp'),
 
 var messages = {
   jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+}
 
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
+function jekyllbuild (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build', '--config=_config.yml'], {stdio: 'inherit'})
         .on('close', done);
-});
+}
 
 /**
  * Build the dev Jekyll Site
  */
- gulp.task('jekyll-build-dev', function (done) {
+function jekyllbuilddev (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build', '--config=_config.yml,_config-dev.yml'], {stdio: 'inherit'})
         .on('close', done);
-});
+}
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['styles', 'jekyll-build'], function() {
+function browsersync(cb) {
   browserSync.init({
     server: {
       baseDir: '_site'
     },
     startPath: "/index.html"
   });
-});
+  cb()
+}
 
-// To support opacity in IE 8
-
-var opacity = function(css) {
-  css.eachDecl(function(decl, i) {
-    if (decl.prop === 'opacity') {
-      decl.parent.insertAfter(i, {
-        prop: '-ms-filter',
-        value: '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + (parseFloat(decl.value) * 100) + ')"'
-      });
-    }
-  });
-};
+// var opacity = function(css) {
+//   css.eachDecl(function(decl, i) {
+//     if (decl.prop === 'opacity') {
+//       decl.parent.insertAfter(i, {
+//         prop: '-ms-filter',
+//         value: '"progid:DXImageTransform.Microsoft.Alpha(Opacity=' + (parseFloat(decl.value) * 100) + ')"'
+//       });
+//     }
+//   });
+// };
 
 /**
  * Compile files from sass into both assets/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('styles', function() {
+function styles() {
   return gulp.src('_scss/main.scss')
     .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(autoprefixer({browsers: ['last 2 versions', 'Firefox ESR', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1']}))
-    .pipe(postcss([opacity]))
+    .pipe(autoprefixer())
+    //.pipe(postcss([opacity]))
     .pipe(gulp.dest('assets/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('assets/css'));
-});
+};
 
 /**
  * Automatically resize post feature images and turn them into thumbnails
  */
-gulp.task("thumbnails", function () {
-  gulp.src("assets/images/hero/*.{jpg,png}")
+function thumbnails () {
+  return gulp.src("assets/images/hero/*.{jpg,png}")
     .pipe(parallel(
       imageResize({ width : 350 }),
       os.cpus().length
     ))
     .pipe(gulp.dest("assets/images/thumbnail"));
-});
+};
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll
  * Watch _site generation, reload BrowserSync
  */
-gulp.task('watch', function() {
-  gulp.watch('_scss/**/*.scss', ['styles']);
-  gulp.watch('assets/images/hero/*.{jpg,png}', ['thumbnails']);
+function watch() {
+  gulp.watch('_scss/**/*.scss', styles);
+  gulp.watch('assets/images/hero/*.{jpg,png}', thumbnails);
   gulp.watch(['*.html',
           '*.txt',
           'about/**',
@@ -102,17 +100,24 @@ gulp.task('watch', function() {
           '_includes/**',
           'assets/css/**'
         ],
-        ['jekyll-build']);
+        jekyllbuild);
   gulp.watch("_site/index.html").on('change', browserSync.reload);
-});
+}
 
-gulp.task('build', ['thumbnails', 'styles', 'jekyll-build-dev', 'jekyll-build']);
+var build = gulp.series(thumbnails, styles, jekyllbuilddev,jekyllbuild);
 
-gulp.task('dev', ['thumbnails', 'browser-sync', 'jekyll-build-dev', 'watch']);
+var dev = gulp.series(thumbnails, styles, jekyllbuild, browsersync, watch);
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 
- gulp.task('default', ['dev']);
+exports.styles = styles;
+exports.thumbnails = thumbnails;
+exports.jekyllbuild = jekyllbuild;
+exports.jekyllbuilddev = jekyllbuilddev;
+exports.watch = watch;
+exports.dev = dev;
+exports.build = build;
+exports.default = dev;
